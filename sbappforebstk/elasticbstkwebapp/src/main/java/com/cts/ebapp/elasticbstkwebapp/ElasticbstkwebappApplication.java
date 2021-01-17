b83bcbb899cc647be37b6bfa0fd586a2855c4dc0
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2021-01-17 20:22:58 +0000

    NA

---
+++ sbappforebstk/elasticbstkwebapp/src/main/java/com/cts/ebapp/elasticbstkwebapp/ElasticbstkwebappApplication.java
@@ -0,0 +1,13 @@
+package com.cts.ebapp.elasticbstkwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticbstkwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticbstkwebappApplication.class, args);
+	}
+
+}
---
+++ sbappforebstk/elasticbstkwebapp/src/test/java/com/cts/ebapp/elasticbstkwebapp/ElasticbstkwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.ebapp.elasticbstkwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticbstkwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
